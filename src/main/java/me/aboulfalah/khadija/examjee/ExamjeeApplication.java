d23930d634b2763d82fbb50767ac3ce661776298
ABOULFALAH Khadija <aboulfalah12@gmail.com>
2026-05-11 14:36:57 +0100

    first commit

---
+++ src/main/java/me/aboulfalah/khadija/examjee/ExamjeeApplication.java
@@ -0,0 +1,13 @@
+package me.aboulfalah.khadija.examjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamjeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamjeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/aboulfalah/khadija/examjee/ExamjeeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aboulfalah.khadija.examjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamjeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
